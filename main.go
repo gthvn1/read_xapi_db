38d6e44ce0708de52b0a188c336c9cb590a8bf88
Guillaume <guillaume.thouvenin@vates.tech>
2025-11-18 18:21:52 +0100

    Transform XML to a tree

--- main.go
+++ main.go
@@ -1,71 +1,29 @@
 package main
 
 import (
-	"encoding/xml"
 	"fmt"
+	"io"
 	"os"
+
+	"example.com/readxapidb/xapidb"
 )
 
 var XAPIDB = "./xapi-db.xml"
 
-func checkOrDie(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-// Example of DB:
-//
-// <database>
-//  <table name="Bond"/>
-//  <table name="PCI">
-//   <row ref="..." __ctime="..." ... />
-//   <row ref="..." __ctime="..." ... />
-//  </table>
-//  <table name="VDI">
-//   <row ref="..." ... />
-//  </table>
-// </database>
-
-type Node struct {
-	XMLName  xml.Name
-	Attr     []xml.Attr `xml:",any,attr"`
-	Children []Node     `xml:",any"`
-	InnerXML string     `xml:",innerxml"`
-}
-
 func main() {
 	data, err := os.ReadFile(XAPIDB)
-	checkOrDie(err)
-	fmt.Printf("Read %d bytes from %s\n", len(data), XAPIDB)
-
-	var root Node
-	checkOrDie(xml.Unmarshal(data, &root))
-
-	fmt.Println("Root element: ", root.XMLName.Local)
-
-	for _, table := range root.Children {
-		if table.XMLName.Local != "table" {
-			continue
-		}
-
-		var tableName string
-		for _, a := range table.Attr {
-			if a.Name.Local == "name" {
-				tableName = a.Value
-			}
-		}
-
-		for _, row := range table.Children {
-			if row.XMLName.Local != "row" {
-				continue
-			}
-
-			fmt.Printf("Row in table %s:\n", tableName)
-			for _, a := range row.Attr {
-				fmt.Printf("  %s = %s\n", a.Name.Local, a.Value)
-			}
-		}
+	if err != nil {
+		fmt.Printf("failed to read %s: %s\n", XAPIDB, err)
+		os.Exit(1)
 	}
 
+	fmt.Printf("Read %d bytes from %s\n", len(data), XAPIDB)
+
+	t, parse_err := xapidb.ParseXapiDB(data)
+	if parse_err != nil && parse_err != io.EOF {
+		fmt.Println("failed to parse XAPI DB:", parse_err)
+		os.Exit(2)
+	}
+
+	xapidb.PrintTree(t)
 }
---
+++ xapidb/parse.go
@@ -0,0 +1,186 @@
+package xapidb
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"strings"
+)
+
+// Example of DB:
+//
+// <database>
+//  <table name="Bond"/>
+//  <table name="PCI">
+//   <row ref="..." __ctime="..." ... />
+//   <row ref="..." __ctime="..." ... />
+//  </table>
+//  <table name="VDI">
+//   <row ref="..." ... />
+//  </table>
+// </database>
+
+// XMLName  -> the element name (database, table, row)
+// Attr     -> All attributes of the element (for table it is name))
+// Children -> all sub-element (for database it is table)
+//
+// So for the example we have:
+//
+//     Node (database)
+//     ├── XMLName = "database"
+//     ├── Attr = []
+//     ├── Children = [
+//     │
+//     │   Node (table)
+//     │   ├── XMLName = "table"
+//     │   ├── Attr = [ name="Bond" ]
+//     │   ├── Children = []
+//     │   └── InnerXML = ""
+//     │
+//     │   Node (table)
+//     │   ├── XMLName = "table"
+//     │   ├── Attr = [ name="PCI" ]
+//     │   ├── Children = [
+//     │   │
+//     │   │   Node (row)
+//     │   │   ├── XMLName = "row"
+//     │   │   ├── Attr = [ ref="...", __ctime="...", ... ]
+//     │   │   ├── Children = []
+//     │   │   └── InnerXML = ""
+//     │   │
+//     │   │   Node (row)
+//     │   │   ├── XMLName = "row"
+//     │   │   ├── Attr = [ ref="...", __ctime="...", ... ]
+//     │   │   ├── Children = []
+//     │   │   └── InnerXML = ""
+//     │   ]
+//     │   └── InnerXML = "<row .../> <row .../>"
+//     │
+//     │   Node (table)
+//     │   ├── XMLName = "table"
+//     │   ├── Attr = [ name="VDI" ]
+//     │   ├── Children = [
+//     │   │   Node (row)
+//     │   │   ├── XMLName = "row"
+//     │   │   ├── Attr = [ ref="..." ]
+//     │   │   ├── Children = []
+//     │   │   └── InnerXML = ""
+//     │   ]
+//     │   └── InnerXML = "<row .../>"
+//     ]
+//     └── InnerXML = "<table .../> <table ...>...</table> ..."
+
+// Using the parser we want to produce this tree:
+//
+//  Node(database)                           <-- root
+//  ├─ Node(table)                            <-- table name="Bond"
+//  │   Attr: { "name": "Bond" }
+//  │   Children: []
+//  │
+//  ├─ Node(table)                            <-- table name="PCI"
+//  │   Attr: { "name": "PCI" }
+//  │   Children:
+//  │     ├─ Node(row)
+//  │     │   Attr: { "ref": "...", "__ctime": "...", ... }
+//  │     │   Children: []
+//  │     │
+//  │     └─ Node(row)
+//  │         Attr: { "ref": "...", "__ctime": "...", ... }
+//  │         Children: []
+//  │
+//  └─ Node(table)                            <-- table name="VDI"
+//      Attr: { "name": "VDI" }
+//      Children:
+//        └─ Node(row)
+//            Attr: { "ref": "...", ... }
+//            Children: []
+
+type Node struct {
+	Name     string
+	Attr     map[string]string
+	Children []*Node
+	Parent   *Node // Will be usefull to deal with "cd .."
+}
+
+func PrintTree(n *Node) {
+	var print func(node *Node, prefix string)
+
+	print = func(node *Node, prefix string) {
+		// Print node name
+		fmt.Print(prefix)
+		fmt.Print(node.Name)
+
+		// Print attributes if any
+		if len(node.Attr) > 0 {
+			attrs := []string{}
+			for k, v := range node.Attr {
+				attrs = append(attrs, fmt.Sprintf(`%s="%s"`, k, v))
+			}
+			fmt.Print(" [", strings.Join(attrs, " "), "]")
+		}
+		fmt.Println()
+
+		// Recurse into children
+		for _, child := range node.Children {
+			print(child, prefix+"  ") // indent 2 spaces per level
+		}
+	}
+
+	print(n, "")
+}
+
+func ParseXapiDB(data []byte) (*Node, error) {
+	decoder := xml.NewDecoder(bytes.NewReader(data))
+
+	var stack []*Node
+	var root *Node
+
+	for {
+		// Get the next XML token in the input stream
+		tok, err := decoder.Token()
+		if err != nil {
+			// We reach the end of bytes
+			if err == io.EOF {
+				break
+			}
+			// Otherwise it is an error
+			return nil, err
+		}
+
+		switch t := tok.(type) {
+		case xml.StartElement:
+			fmt.Println("Found start element", t.Name.Local)
+			// Create node only for table
+			n := &Node{
+				Name:     t.Name.Local,
+				Attr:     map[string]string{},
+				Children: []*Node{},
+			}
+
+			// Set attributes
+			for _, a := range t.Attr {
+				fmt.Printf("  set %s -> %s\n", a.Name.Local, a.Value)
+				n.Attr[a.Name.Local] = a.Value
+			}
+
+			// Attach to parent if not root
+			if len(stack) > 0 {
+				parent := stack[len(stack)-1]
+				n.Parent = parent
+				parent.Children = append(parent.Children, n)
+			} else {
+				fmt.Println("Setting root to", n.Name)
+				root = n
+			}
+
+			stack = append(stack, n)
+
+		case xml.EndElement:
+			fmt.Println("Found end element", t.Name.Local)
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return root, nil
+}
