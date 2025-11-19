0af8ec399c04f4f8cb7bd29ce1c7cadaebe6d1f1
Guillaume <gthvn1@gmail.com>
2025-11-19 20:53:17 +0100

    Use tview tree to display the database
    Start from TreeView example of the wiki and modify it to use our own DB tree.
    Display the tree on the left and the status on the right that will contain all information.

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"io"
 	"os"
 
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+
 	"example.com/readxapidb/xapidb"
 )
 
@@ -19,11 +22,120 @@ func main() {
 
 	fmt.Printf("Read %d bytes from %s\n", len(data), XAPIDB)
 
-	t, parse_err := xapidb.ParseXapiDB(data)
+	rootNode, parse_err := xapidb.ParseXapiDB(data)
 	if parse_err != nil && parse_err != io.EOF {
-		fmt.Println("failed to parse XAPI DB:", parse_err)
+		fmt.Printf("failed to parse %s: %s\n", XAPIDB, parse_err)
 		os.Exit(2)
 	}
 
-	xapidb.PrintTree(t)
+	//xapidb.PrintTree(t)
+
+	// Instead of printing the tree we will try to use the demo of navigable
+	// tree view of current dir: https://github.com/rivo/tview/wiki/TreeView
+	rootTree := makeTreeNode(rootNode)
+	tree := tview.NewTreeView().
+		SetRoot(rootTree).
+		SetCurrentNode(rootTree)
+	// Set border and title are done separatly otherwise the type of tree is
+	// modified to tview.Box instead of TreeView !!!
+	tree.SetBorder(true).SetTitle("XAPI DB")
+
+	// We add a status view to print all row attributes for example
+	status := tview.NewTextView().
+		SetDynamicColors(true)
+	status.SetBorder(true).SetTitle("Status")
+
+	// If a directory was selected, open it.
+	tree.SetSelectedFunc(func(tn *tview.TreeNode) {
+		ref := tn.GetReference()
+		if ref == nil {
+			return // Selecting the root node does nothing.
+		}
+
+		node := ref.(*xapidb.Node)
+
+		// Update status
+		updateStatus(status, node)
+
+		// Load children only once
+		children := tn.GetChildren()
+		if len(children) == 0 {
+			for _, c := range node.Children {
+				tn.AddChild(makeTreeNode(c))
+			}
+		}
+		// Collapse if visible, expand if collapsed.
+		tn.SetExpanded(!tn.IsExpanded())
+	})
+
+	layout := tview.NewFlex().
+		SetDirection(tview.FlexColumn).
+		AddItem(tree, 0, 1, true).
+		AddItem(status, 40, 0, false)
+
+	if err := tview.NewApplication().SetRoot(layout, true).Run(); err != nil {
+		panic(err)
+	}
+}
+
+func makeTreeNode(n *xapidb.Node) *tview.TreeNode {
+	label := n.Name
+
+	// If there is a name attribute add it (it is the case for table)
+	if name, ok := n.Attr["name"]; ok {
+		label += fmt.Sprintf(" (%s)", name)
+	}
+
+	// If there is a ref add it (it is the case for row)
+	if ref, ok := n.Attr["ref"]; ok {
+		label += fmt.Sprintf(" [ref=%s]", ref)
+	}
+
+	t := tview.NewTreeNode(label)
+	t.SetReference(n) // This maps the tree view with our node
+	t.SetSelectable(true)
+
+	switch n.Name {
+	case "database":
+		t.SetColor(tcell.ColorRed)
+	case "table":
+		t.SetColor(tcell.ColorGreen)
+	case "row":
+		t.SetColor(tcell.ColorBlue)
+	default:
+		t.SetColor(tcell.ColorWhite)
+	}
+
+	// Just create the node, we will add children later
+	return t
+}
+
+func updateStatus(tv *tview.TextView, n *xapidb.Node) {
+	tv.Clear()
+
+	fmt.Fprintf(tv, "[yellow]Name:[white] %s\n", n.Name)
+
+	if len(n.Attr) > 0 {
+		fmt.Fprintf(tv, "[yellow]Attributes:[white]\n")
+		for k, v := range n.Attr {
+			fmt.Fprintf(tv, "  %s = %q\n", k, v)
+		}
+	} else {
+		fmt.Fprintf(tv, "[yellow]Attributes:[white] (none)\n")
+	}
+
+	fmt.Fprintf(tv, "[yellow]Children:[white] %d\n", len(n.Children))
+
+	// Compute path
+	path := ""
+	cur := n
+	for cur != nil {
+		if cur.Parent != nil {
+			path = "/" + cur.Name + path
+		} else {
+			path = "/database" + path
+		}
+		cur = cur.Parent
+	}
+	fmt.Fprintf(tv, "[yellow]Path:[white] %s\n", path)
 }
