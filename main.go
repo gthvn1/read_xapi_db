c7937ba9513da06742b46a5f06ba971204a0d67e
Guillaume <guillaume.thouvenin@vates.tech>
2025-11-21 13:40:11 +0100

    Print the name instead of OpaqueRef when available

--- main.go
+++ main.go
@@ -218,25 +218,25 @@ func makeTreeNode(n *xapidb.Node) *tview.TreeNode {
 
 	// If there is a name attribute add it (it is the case for table)
 	if name, ok := n.Attr["name"]; ok {
-		label += fmt.Sprintf(" (%s) %d", name, len(n.Children))
+		label += fmt.Sprintf(" (%s)", name)
 	}
 
-	// If there is a ref add it (it is the case for row)
-	if ref, ok := n.Attr["ref"]; ok {
-		label += fmt.Sprintf(" [ref=%s]", ref)
+	// If there is children print the number so you will know which
+	// node can be unfold
+	if len(n.Children) > 0 {
+		label += fmt.Sprintf(" %d", len(n.Children))
+	}
+
+	// If there is a name__label add it, it not check if there is a ref.
+	if nameLabel, ok := n.Attr["name__label"]; ok && len(nameLabel) > 0 {
+		label += fmt.Sprintf(" [%s]", nameLabel)
+	} else if ref, ok := n.Attr["ref"]; ok {
+		label += fmt.Sprintf(" [%s]", ref)
 	}
 
 	tn := tview.NewTreeNode(label)
 	tn.SetReference(n) // This maps the tree view with our node
 	tn.SetSelectable(true)
-
-	if len(n.Children) > 0 {
-		tn.SetExpanded(false) // This should show expandable sign
-		tn.SetText("[yellow]+[white] " + label)
-	} else {
-		tn.SetExpanded(true) // This should show expandable sign
-		tn.SetText("[white]-[white] " + label)
-	}
 
 	switch n.Name {
 	case "database":
